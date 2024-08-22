1dc161a7e8cad31c0367018689749e7b1355c96a
Cavalcantiipedro <pedrocavalcanti.r@gmail.com>
2024-08-22 09:31:32 -0300

    começando a entender

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ botaoSubmeter.addEventListener("click",submeter)
 
 let nome = document.getElementById("nome").ariaValueMax;
 
+let idade = document.getElementById("idade").ariaValueMax
+
+let cpf = document.getElementById("cpf").ariaValueMax
+
+let data = document.getElementById("data").ariaValueMax
 console.log(nome);
 
 function submeter() {
-   
+    let nome = document.getElementById("nome").value;
+    console.log(nome);
+
+    let idade = document.getElementById("idade").value;
+    console.log(idade);
+
+    let cpf = document.getElementById("cpf").value;
+    console.log(cpf);
+
+    let data = document.getElementById("data").value;
+    console.log(data);
+
 }
