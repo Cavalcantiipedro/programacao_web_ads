26c72236a61c68cd52db22388788ffd2f61dcf05
Cavalcantiipedro <pedrocavalcanti.r@gmail.com>
2024-08-22 09:14:51 -0300

    cheguei atrasado e to tentando entender

---
+++ index.js
@@ -0,0 +1,13 @@
+let pagina = document;
+
+let botaoSubmeter = document.getElementById("botao-s")
+
+botaoSubmeter.addEventListener("click",submeter)
+
+let nome = document.getElementById("nome").ariaValueMax;
+
+console.log(nome);
+
+function submeter() {
+   
+}
